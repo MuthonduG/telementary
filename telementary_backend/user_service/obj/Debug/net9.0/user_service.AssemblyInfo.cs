267e70b309adefbf95e2110e9140ffd8231d2ccc
MuthonduG <muthondugithinji@gmail.com>
2025-03-02 11:25:45 -0500

    updates: updated lib file structure

--- telementary_backend/user_service/obj/Debug/net9.0/user_service.AssemblyInfo.cs
+++ telementary_backend/user_service/obj/Debug/net9.0/user_service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("user_service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efcb178a05dbff6409d666fc2e8840d96402f3c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("user_service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("user_service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
